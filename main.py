135b7907ec8057c0871b2159ead2e5a6914b4896
Chanchal Roy <codewithcc.dev@gmail.com>
2026-05-17 23:41:27 +0530

    Adding mongodb CRUD operation in our API endpoints

--- main.py
+++ main.py
@@ -1,12 +1,99 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pymongo
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+from bson import ObjectId
+from bson.errors import InvalidId
 
 app = FastAPI()
+load_dotenv()
+client = pymongo.MongoClient(os.getenv("MONGODB_URI"))
+db = client.get_database("ai-todo-app")
+collection = db.get_collection("todos")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Todo(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    checked: bool | None = None
 
+@app.get("/todos")
+def get_todos():
+    try:
+        data = []
+        for doc in collection.find():
+            doc["_id"] = str(doc["_id"])
+            data.append(doc)
+        return {"success": True, "data": data}
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(500, "Internal server error")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: str):
+    try:
+        try: obj_id = ObjectId(todo_id)
+        except InvalidId: raise HTTPException(400, "Given object-id is invalid.")
+
+        data = collection.find_one({"_id": obj_id})
+        if not data: raise HTTPException(404, f"No todo found with id:{todo_id}")
+
+        data["_id"] = str(data["_id"])
+        return {"success": True, "data": data}
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(500, "Internal server error")
+
+@app.post("/todos")
+def create_todo(todo: Todo):
+    try:
+        raw_data = {k: v for k, v in todo.model_dump().items() if v is not None}
+        if not raw_data: raise HTTPException(400, "Given request body containing null values!")
+
+        raw_data["createdAt"] = datetime.now(timezone.utc).isoformat()
+        raw_data["updatedAt"] = datetime.now(timezone.utc).isoformat()
+        data = collection.insert_one(raw_data)
+
+        return {"success": True, "message": f"New todo inserted with id:{data.inserted_id}"}
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(500, "Internal server error")
+
+@app.patch("/todos/{todo_id}")
+def update_todo(todo_id: str, todo: Todo):
+    try:
+        try: obj_id = ObjectId(todo_id)
+        except InvalidId: raise HTTPException(400, "Given object-id is invalid.")
+
+        raw_data = {k: v for k, v in todo.model_dump().items() if v is not None}
+        if not raw_data: raise HTTPException(400, "Given request body containing null values!")
+
+        raw_data["updatedAt"] = datetime.now(timezone.utc).isoformat()
+        data = collection.find_one_and_update({"_id": obj_id}, {"$set": raw_data})
+
+        if data.matched_count == 0: raise HTTPException(404, f"No item found with id:{todo_id}.")
+
+        return {"success": True, "message": f"Todo with ID:{todo_id} updated!"}
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(500, "Internal server error")
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: str):
+    try:
+        try: obj_id = ObjectId(todo_id)
+        except InvalidId: raise HTTPException(400, "Given object-id is invalid.")
+
+        data = collection.find_one_and_delete({"_id": obj_id})
+        if data.deleted_count == 0: raise HTTPException(404, f"No item found with id:{todo_id}.")
+
+        return {"success": True, "message": f"Todo with ID:{todo_id} deleted!"}
+    except HTTPException as e:
+        raise e
+    except Exception:
+        raise HTTPException(500, "Internal server error")
